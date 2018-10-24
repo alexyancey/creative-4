4d16b3941a60af3fa74656af7bcc4621b6526160
Alex Yancey <james.alexander.yancey@gmail.com>
2018-10-24 09:42:26 -0700

    socket.io

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const port = 4000;
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -9,4 +11,7 @@ app.get('/', function(req, res) {
     res.sendFile('index.html', { root: path.join(__dirname, 'views') });
 });
 
-app.listen(port, () => console.log('Creative Project 4 listening on port 4000!'));
+http.listen(4000, function() {
+    console.log('Creative Project 4 listening on port 4000!');
+})
+//app.listen(port, () => console.log('Creative Project 4 listening on port 4000!'));
